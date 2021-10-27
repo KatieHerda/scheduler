c8eb78fc95a7c898813ad592e3ab300f0afb10be
Katie Herda <87781887+KatieHerda@users.noreply.github.com>
2021-10-27 17:44:41 -0600

    Added Show, Header, Empty, and Confirm portions with accompanying stories.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import classNames from 'classnames';
 import "./styles.scss";
 
 
 export default function Appointment(props) {
-
   return (
     <article className="appointment"> {props.time ? "Appointment at" + props.time : "No Appointment" }</article>
   )
