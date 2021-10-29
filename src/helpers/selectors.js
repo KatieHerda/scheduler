5231a5fa6473e62e19786b8435fa4522148968f4
Katie Herda <87781887+KatieHerda@users.noreply.github.com>
2021-10-28 19:20:43 -0600

    added helper function to return an array of appointment data for a given day.

---
+++ src/helpers/selectors.js
@@ -0,0 +1,16 @@
+export function getAppointmentsForDay(state, dayName) {
+  const foundDay = state.days.find((day) => day.name === dayName);
+
+  //if no day found, return empty array
+  if (!foundDay) {
+    return [];
+  }
+
+  //return an array of the appointments for the day
+  const result = foundDay.appointments.map((Id) => {
+    return state.appointments[Id];
+  });
+
+  //return final array
+  return result;
+}
