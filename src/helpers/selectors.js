75ddbdf3ce7157e281f1cbe6f55f8845d6a3f22c
Katie Herda <87781887+KatieHerda@users.noreply.github.com>
2021-10-30 15:14:08 -0600

    added a getInterviewersForDay function with testing.

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,4 @@
+// // GET APPTS FOR DAY // //
 export function getAppointmentsForDay(state, dayName) {
   const foundDay = state.days.find((day) => day.name === dayName);
 
@@ -13,8 +14,26 @@ export function getAppointmentsForDay(state, dayName) {
 
   //return final array
   return result;
-};
+}
 
+// // GET INTERVIEWERS FOR DAY // //
+export function getInterviewersForDay(state, dayName) {
+  const foundDay = state.days.find((day) => day.name === dayName);
+
+  if (!foundDay) {
+    return [];
+  }
+
+  //return an array of the interviewers for the day
+  const result = foundDay.interviewers.map((Id) => {
+    return state.interviewers[Id];
+  });
+
+  //return final array
+  return result;
+}
+
+// // GET INTERVIEW // //
 export function getInterview(state, interview) {
   if (!interview) {
     return null;
@@ -22,13 +41,8 @@ export function getInterview(state, interview) {
 
   const interviewerId = interview.interviewer;
 
-  //if no interview is booked, return null
-
-  // console.log("interviewer", state.interviewers)
-  // console.log("interviewerId", interviewerId)
-
-
   return {
-    ...interview, interviewer: state.interviewers[interviewerId]
-  }
+    ...interview,
+    interviewer: state.interviewers[interviewerId],
+  };
 }
