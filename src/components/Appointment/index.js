9390ba592e0ba6a5aab00ceea8f7cf1325fbe88d
Katie Herda <87781887+KatieHerda@users.noreply.github.com>
2021-11-01 21:29:29 -0600

    Implimented .catch error handling for save and delete if axios rejects their respective functions. Added boolean (true) to the catch statement to replace current mode so when exit x clicked, we return to appropriate places in history.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,6 +5,7 @@ import Empty from './Empty';
 import Form from './Form';
 import Status from './Status';
 import Confirm from './Confirm';
+import Error from './Error';
 import useVisualMode from 'hooks/useVisualMode';
 
 import "./styles.scss";
@@ -14,30 +15,35 @@ export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const EDIT = "EDIT";
   const SAVING = "SAVING";
-  const DELETING = "DELETING"
-  const CONFIRMING = "CONFIRMING"
+  const DELETING = "DELETING";
+  const CONFIRMING = "CONFIRMING";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
   
   const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
 
+  //True - current mode replaced, taking user to create, instead of status component.
   function save(name, interviewer) {
     const interview = {
       student: name, 
       interviewer
     }
     transition(SAVING)
-    props.bookInterview(props.id, interview)
-      .then((res) => {
-        transition(SHOW)
-      })
+    props
+      .bookInterview(props.id, interview)
+      .then(() => transition(SHOW))
+      .catch(error => transition(ERROR_SAVE, true))
   }
 
+  //True - current mode reaplced, taking user directly back to show mode, instead of confirm component.
   function deleteInterview() {
-    transition(DELETING)
-    props.cancelInterview(props.id)
-      .then((res) => {
-        transition(EMPTY)
-      })
+    transition(DELETING, true)
+    props
+      .cancelInterview(props.id)
+      .then(() => transition(EMPTY))
+      .catch(error => transition(ERROR_DELETE, true))
   }
 
   return (
@@ -49,27 +55,16 @@ export default function Appointment(props) {
         student={props.interview.student}
         interviewer={props.interview.interviewer}
         onDelete={() => transition(CONFIRMING)}
+        onEdit={() => transition(EDIT)}
         />
       )}
-      {mode === CREATE && 
-      <Form 
-      interviewers={props.interviewers}
-      onCancel={() => back()}
-      onSave={save}
-      />}
-      {mode === SAVING && (
-        < Status message="Saving" />
-      )}
-      {mode === DELETING && (
-        < Status message="Deleting" />
-      )}
-      {mode === CONFIRMING && (
-        < Confirm 
-        message="Are you sure you would like to delete?" 
-        onCancel={back}
-        onConfirm={deleteInterview}
-        />
-      )}
+      {mode === CREATE && (< Form interviewers={props.interviewers} onCancel={() => back()} onSave={save}/>)}
+      {mode === EDIT && < Form student={props.interview.student} interviewer={props.interview.interviewer.id} interviewers={props.interviewers} onSave={save} onCancel={back}/>}
+      {mode === SAVING && (< Status message="Saving" />)}
+      {mode === DELETING && (< Status message="Deleting" />)}
+      {mode === CONFIRMING && (< Confirm message="Are you sure you would like to delete?" onCancel={back} onConfirm={deleteInterview}/>)}
+      {mode === ERROR_SAVE && (< Error message="Could not save appointment." onClose={back} />)}
+      {mode === ERROR_DELETE && (< Error message="Could not delete appointment." onClose={back} />)}
     </article>
   );
 }
