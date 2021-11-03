e5892a07fa0381c410564f2debf8d1d2043bd0d6
Katie Herda <87781887+KatieHerda@users.noreply.github.com>
2021-11-03 13:48:14 -0600

    added Form tests to check if it can render without student name if not provided, validates that a student name cannot be blank, and calls onSave function when a name is defined.

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -5,40 +5,41 @@ import React from "react";
   We import our helper functions from the react-testing-library
   The render function allows us to render Components
 */
-import { render } from "@testing-library/react"
-
+import { render, cleanup } from "@testing-library/react";
 
 //We import the component that we are testing
 import Appointment from "components/Appointment";
+
+afterEach(cleanup);
 
 //Interviewers data to pass to interviewers in appProps
 const interviewers = [
   {
     id: 1,
     avatar: "https://i.imgur.com/LpaY82x.png",
-    name: "Sylvia Palmer"
+    name: "Sylvia Palmer",
   },
   {
     id: 4,
     avatar: "https://i.imgur.com/FK8V841.jpg",
-    name: "Cohana Roy"
+    name: "Cohana Roy",
   },
   {
     id: 6,
     avatar: "https://i.imgur.com/TdOAdde.jpg",
-    name: "Susan Reynolds"
-  }
-] 
+    name: "Susan Reynolds",
+  },
+];
 
 //Fake data to pass as props
 const appProps = {
-  id: 6, 
-  time: '12pm', 
-  interview: null, 
+  id: 6,
+  time: "12pm",
+  interview: null,
   interviewers: interviewers,
   cancelInterview: () => {},
-  bookInterview: () => {}
-}
+  bookInterview: () => {},
+};
 
 /*
 We wrap related tests in a describe function
@@ -46,8 +47,7 @@ If props are required for your component, you need to fake them
 */
 describe("Appointment", () => {
   // A test that renders a React Component
-  it('renders without crashing', () => {
-    render(< Appointment {...appProps} />);
+  it("renders without crashing", () => {
+    render(<Appointment {...appProps} />);
   });
-
-})
+});
